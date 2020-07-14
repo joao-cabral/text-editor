6d6d6f23e9e076d697ec711005d2572d1d67e3b5
Joao Cabral <joaocabral6665@gmail.com>
2020-07-13 21:08:06 -0400

    Project static

---
+++ text-editor-static/precache-manifest.2b20aaf56b64ac193d63c092c2fc46fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120df92c99e348fe6e1c3674fa523abb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cbbbc37f20d48fb23c5",
+    "url": "/static/css/main.93c6ded5.chunk.css"
+  },
+  {
+    "revision": "d4fb3d337dae24ee8a29",
+    "url": "/static/js/2.a47c34e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a47c34e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbbbc37f20d48fb23c5",
+    "url": "/static/js/main.5fef7b24.chunk.js"
+  },
+  {
+    "revision": "3689250a28c5ba21b97b",
+    "url": "/static/js/runtime-main.ec6f72f0.js"
+  }
+]);
